46fd350169ff95f33e4fae4551a37cb0a6140b3a
yz9797 <47649811+yz9797@users.noreply.github.com>
2019-03-24 21:02:51 -0400

    Update Boisson.h

--- TP4/Boisson.h
+++ TP4/Boisson.h
@@ -13,9 +13,26 @@ class Boisson: public Taxable
 {
  public:
     Boisson (string_view nom, double prix);
+    /**
+	 * @brief getNom, donne le nom de boisson
+	 * @return le nom_
+	 */
     string_view getNom() const;
+    
+	/**
+	 * @brief getPrix, donne le prix de boisson
+	 * @return le prix_
+	 */
     double getPrix () const;
+    	 
+/**
+	 * @brief setaxe, met le taxe 
+	 */
 	void setTaxe() override;
+	/**
+	 * @brief getTaxe, donne le taxe de boisson
+	 * @return le taxe,
+	 */
 	double getTaxe() const override;
 
 protected:
