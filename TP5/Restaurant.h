9040c976919b92a3419deba45f25c0e717513e24
yz9797 <47649811+yz9797@users.noreply.github.com>
2019-04-07 15:47:18 -0400

    Update Restaurant.h

--- TP5/Restaurant.h
+++ TP5/Restaurant.h
@@ -34,16 +34,41 @@ public:
 	~Restaurant();
 
 	// Setters.
-
+	/**
+	 * @brief mettre le type de menu
+	 * @param moment, le type de menu
+	 */
 	void setMoment(TypeMenu moment);
+	/**
+	 * @brief mettre le nom du restaurant
+	 * @param nom, le nom à assigner
+	 */
 	void setNom(string_view nom); 
+	/**
+	 * @brief mettre le chiffre d'affaires du restaurant
+	 * @param chiffre, le chiffre à assigner
+	 */
     void setChiffreAffaire( double chiffre);
 	// Getters.
-
+	/**
+	 * @return le nom du restaurant
+	 */
 	string getNom() const;
+	/**
+	 * @return le moment de la journee (le type de menu)
+	 */
 	TypeMenu getMoment() const;
+	/**
+	 * @return le chiffre d'affaires
+	 */
     double getChiffreAffaire();
+	/**
+	 * @brief getTables, retourne le gestionnaire de tables
+	 */
 	GestionnaireTables* getTables() const;
+	/**
+	 * @brief getTables, retourne le gestionnaire de plats
+	 */
 	GestionnairePlats* getMenu(TypeMenu typeMenu) const; 
 	double getFraisLivraison(ZoneHabitation zone) const;
 	string getNomTypeMenu(TypeMenu typeMenu) const;
@@ -54,14 +79,37 @@ public:
 
 	//void lireTables(const string& fichier); // TODO : À retirer, déplcer dans GestionnaitreTables.
 	//Restaurant& operator+=(owner<Table*> table); // TODO : À retirer, deplacer dans GestionnaireTables //? Le param�tre est indiqu� "owner" pour montrer que cette m�thode prend possession du pointeur qui lui est pass�, mais owner<Table*> est �quivalent � Table*; voir pointers.h .
-
+	/**
+	 * @brief commande un plat à une table
+	 * @param nom, le nom du plat à commander
+	 * @param idTable, l id de la table
+	 */
 	void commanderPlat(const string& nom, int idTable);
+	/**
+	 * @brief operator< pour comparer le chiffre d'affaires des 2 restaurants
+	 *
+	 * @param restau, le restaurant dont on veut comparer avec.
+	 */
 	bool operator <(const Restaurant& restau) const ;
 	Restaurant& operator=(const Restaurant& autre) = delete;  //? Le programme ne fait jamais de copie de restaurant, donc cette fonction n'�tait pas test�e et par cons�quent probablement �rron�e.
-	
+	/**
+	 * @brief place un client dans une table d'une facon optimale
+	 * @param client, pointeur vers le client principal de la table
+	 * @return true si le cilent est placé sinon false
+	 */
 	bool placerClients(Client* client);
+	/**
+	 * @brief livre la commande d'un client en le placant dans la table de livraison
+	 * @param client, pointeur vers le client 
+	 * @param nomPlats, la liste des plats que le client a commandé 
+	 */
 	bool livrerClient(Client* client, const vector<string>& nomPlats);
-	
+	/**
+	 * @brief calculer la réduction pour un client
+	 * @param client, pointeur vers le client principal de la table
+	 * @param montant, facture de la commande
+	 * @param livraison, booléen à 1 si une livraison est faite pour le client
+	 */
 	double calculerReduction(Client* client, double montant, bool livraison);
 	friend ostream& operator<<(ostream& os, const Restaurant& restau);
 	
