b04061c6ca1ba52d4c3ab3ca06c604689670cf09
yz9797 <47649811+yz9797@users.noreply.github.com>
2019-04-07 15:15:01 -0400

    Update GestionnaireTables.h

--- TP5/GestionnaireTables.h
+++ TP5/GestionnaireTables.h
@@ -17,9 +17,22 @@ class GestionnaireTables : public GestionnaireGenerique<Table*, set<Table*>>
 {
 public:
 	GestionnaireTables() = default;
+	/**
+	 * @brief getTable, retourne la table dans le conteneur (qui est de type set) qui porte le id donné en utilisant l'algorithme find_if et une fonction lamda qui compare les id
+	 * @param id, le id de la table cherché
+	 * @return la table cherchée
+	 */
 	Table* getTable(int id) const; 
+	/**
+	 * @brief getMeilleureTable, retourne la meilleure la table qui peut contenir un groupe
+	 * @param tailleGroupe, la taille du groupe
+	 * @return la table cherchée si elle existe
+	 */
 	Table* getMeilleureTable(int tailleGroupe) const;
 	void lireTables(const string& nomFichier);
-
+	/**
+   	* @brief afficherTAbles Pour afficher les tables du conteneur
+     	* @param os, le stream de sortie à afficher.
+     	*/
 	void afficherTables(ostream& os) const;
 };
